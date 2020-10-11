21dadb45a0237840c87c7326dcacc92d7f342132
Mae <maeberris@outlook.com>
2020-10-10 22:21:59 -0400

    added firebase context

---
+++ client/src/components/FirebaseContext.js
@@ -0,0 +1,22 @@
+import React, { createContext } from "react";
+import * as firebase from "firebase";
+
+export const FireBaseContext = createContext(null);
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCvDktR2m3-rIU3ddPsLkqT2Yfp4EASteA",
+  authDomain: "you-beat-me-to-it.firebaseapp.com",
+  databaseURL: "https://you-beat-me-to-it.firebaseio.com",
+  projectId: "you-beat-me-to-it",
+  storageBucket: "you-beat-me-to-it.appspot.com",
+  messagingSenderId: "1075878771170",
+  appId: "1:1075878771170:web:253cca6ef4a7e1e7a8e1bb",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const FireBaseContextProvider = ({ children }) => {
+  return <FireBaseContext.Provider>{children}</FireBaseContext.Provider>;
+};
+
+export default FireBaseContextProvider;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import AppProvider from './components/AppContext';
-import App from './components/App';
+import FireBaseContextProvider from "./components/FirebaseContext";
+import App from "./components/App";
 
 ReactDOM.render(
-  <AppProvider>
+  <FireBaseContextProvider>
     <App />
-  </AppProvider>,
-  document.getElementById('root')
+  </FireBaseContextProvider>,
+  document.getElementById("root")
 );
