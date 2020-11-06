c8d4291b60188652fab61d5dcd4beb6f251b6d86
Mae <maeberris@outlook.com>
2020-11-06 13:43:09 -0500

    added sound bar

--- client/src/components/GameRoom/GameRoom.js
+++ client/src/components/GameRoom/GameRoom.js
@@ -9,6 +9,7 @@ import SearchBar from "./SearchBar";
 import PreviousSong from "./PreviousSongs";
 import * as firebase from "firebase";
 import Ranking from "./Ranking";
+import { FiVolume, FiVolumeX, FiVolume1, FiVolume2 } from "react-icons/fi";
 
 const GameRoom = () => {
   const { roomId } = useParams();
@@ -22,7 +23,13 @@ const GameRoom = () => {
   } = React.useContext(GameRoomContext);
   const { currentUser } = React.useContext(CurrentUserContext);
   const [time, setTime] = React.useState(5);
+  const [volume, setVolume] = React.useState(0.5);
+
   const audioRef = React.useRef(null);
+
+  React.useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [audioRef, volume]);
 
   //This sets the interval for the timer
   React.useEffect(() => {
@@ -122,19 +129,6 @@ const GameRoom = () => {
     }
   }, [gamePhase]);
 
-  //This starts and stops the track
-  // React.useEffect(() => {
-  //   if (trackUrl !== null && gamePhase === "playing") {
-  //     console.log("playTrack");
-  //     audioRef.current.play();
-  //   }
-  //   if (trackUrl !== null && gamePhase === "loading") {
-  //     console.log("stoptrack");
-  //     audioRef.current.pause();
-  //   }
-  // }, [trackUrl, gamePhase]);
-
-  //this gets the current track info
   React.useEffect(() => {
     const currentTrackInfo = firebase
       .database()
@@ -154,8 +148,37 @@ const GameRoom = () => {
 
   return (
     <Wrapper>
+      <SlidderWrapper>
+        <Button
+          onClick={() => {
+            if (volume > 0) {
+              setVolume(0);
+              return;
+            }
+            if (volume === 0) {
+              setVolume(0.5);
+              return;
+            }
+          }}
+        >
+          {volume === 0.5 && <FiVolume size={35} />}
+          {volume === 0 && <FiVolumeX size={35} />}
+          {volume < 0.5 && volume > 0 && <FiVolume1 size={35} />}
+          {volume > 0.5 && <FiVolume2 size={35} />}
+        </Button>
+        <Slider
+          type="range"
+          min="0"
+          max="100"
+          value={volume * 100}
+          onChange={(ev) => {
+            setVolume(ev.target.value / 100);
+          }}
+        />
+      </SlidderWrapper>
+
       <Player src={trackUrl} ref={audioRef}>
-        I'm sorry, your browser doesn't supper audio
+        I'm sorry, your browser doesn't support audio
       </Player>
       <SearchBar />
       <ProgressBar />
@@ -182,6 +205,41 @@ const BottomSection = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: 50% 50%;
+`;
+
+const Slider = styled.input`
+  width: 100%;
+`;
+
+const SlidderWrapper = styled.div`
+  display: flex;
+  padding: 5px;
+  align-items: center;
+  background: white;
+  border-radius: 10px;
+  width: 50px;
+  transition: width 1s, opacity 1s;
+  margin-bottom: 15px;
+
+  input:nth-child(2) {
+    opacity: 0;
+    transition: opacity 0.8s;
+  }
+
+  :hover {
+    width: 200px;
+    input:nth-child(2) {
+      opacity: 1;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  width: 45px;
+  height: 45px;
+  cursor: pointer;
 `;
 
 export default GameRoom;
