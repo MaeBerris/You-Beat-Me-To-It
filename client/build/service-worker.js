6949fbec0410dbf00958a11e6ddc17692957768a
Mae <maeberris@outlook.com>
2020-11-12 14:21:02 -0500

    redeploy after gameOver fix

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9082df1204678cad15a02b2046385760.js"
+  "/precache-manifest.9718a91e56abbcaf375f319f1195bd82.js"
 );
 
 self.addEventListener('message', (event) => {
